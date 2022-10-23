99eaa88cb1286f610fd01f2c5c5789105e646a3b
JohaanMannanal <106918089+JohaanMannanal@users.noreply.github.com>
2022-10-22 20:16:47 -0700

    Update TeleOpCode.java

--- TeleOpCode.java
+++ TeleOpCode.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -53,7 +24,7 @@ import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
 
-public class Test2 extends LinearOpMode {
+public class TeleopCode extends LinearOpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
@@ -115,61 +86,85 @@ public class Test2 extends LinearOpMode {
             boolean clawButtonClose = gamepad2.left_bumper;
             boolean clawButtonOpen = gamepad2.right_bumper;
             
+            boolean elbowUp = gamepad2.x;
+            boolean elbowDown = gamepad2.b;
+            
+            boolean spinUp = gamepad2.y;
+            boolean spinDown = gamepad2.a;
+            
             double frontLeftPower = 0;
             double frontRightPower = 0;
             double backLeftPower = 0;
             double backRightPower = 0;
             
-            double armPower = 0;
+            double armBasePower = 0;
             
             if(y>0){
-                frontLeftPower = 0.59;
+                backRightPower = 0.6;
+                frontLeftPower = 0.58;
                 frontRightPower = 0.6;
                 backLeftPower = 0.6;
-                backRightPower = 0.59;
             } else if(y<0){
-                frontLeftPower = -0.59;
-                frontRightPower = -0.6;
-                backLeftPower = -0.59;
                 backRightPower = -0.6;
+                frontLeftPower = -0.58;
+                frontRightPower = -0.6;
+                backLeftPower = -0.6;
             } 
             
             if(gamepad1.x){
-                frontLeftPower = -0.59;
-                frontRightPower = 0.6;
-                backLeftPower = 0.59;
                 backRightPower = -0.6;
+                frontLeftPower = -0.6;
+                frontRightPower = 0.6;
+                backLeftPower = 0.6;
             }
             
             if(gamepad1.b){
-                frontLeftPower = 0.59;
-                frontRightPower = -0.6;
-                backLeftPower = -0.59;
                 backRightPower = 0.6;
+                frontLeftPower = 0.6;
+                frontRightPower = -0.6;
+                backLeftPower = -0.6;
             }
             
             if(turn>0){
-                frontLeftPower = 0.595;
-                frontRightPower = -0.6;
-                backLeftPower = 0.595;
                 backRightPower = -0.6;
+                frontLeftPower = 0.6;
+                frontRightPower = -0.6;
+                backLeftPower = 0.6;
             } else if(turn<0){
-                frontLeftPower = -0.595;
-                frontRightPower = 0.6;
-                backLeftPower = -0.595;
                 backRightPower = 0.6;
+                frontLeftPower = -0.6;
+                frontRightPower = 0.6;
+                backLeftPower = -0.6;
             }
             
             if(clawButtonClose) {
-                claw.setPosition(0.85f);
+                claw.setPosition(0.85);
             }
             
             if(clawButtonOpen) {
-                claw.setPosition(0.60f);
+                claw.setPosition(0.60);
             }
              
-            if (gamepad1.a){
-                armPower = -0.3;
+            //if (gamepad2.a){
+                //armBasePower = -1;
+            //}
+            
+            if (elbowUp){
+                rightArm.setPosition(1);
+                leftArm.setPosition(0);
+            }
+            
+            if (elbowDown){
+                rightArm.setPosition(0.7);
+                leftArm.setPosition(0.3);
+            }
+            
+            if (spinUp){
+                clawSpin.setPosition(0.07);
+            }
+            
+            if (spinDown){
+                clawSpin.setPosition(0.72);
             }
             
             frontLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -184,7 +179,7 @@ public class Test2 extends LinearOpMode {
             backLeftDrive.setPower(backLeftPower);
             backRightDrive.setPower(backRightPower);
             
-            armBase.setPower(armPower);
+            //armBase.setPower(armBasePower);
 
             // Show the elapsed game time and wheel power.
             //telemetry.addData("Status", "Run Time: " + runtime.toString());
@@ -193,31 +188,3 @@ public class Test2 extends LinearOpMode {
         }
     }
 }
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
